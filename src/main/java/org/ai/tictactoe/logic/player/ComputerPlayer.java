22891d06facdf851bc009978800fbc3b11e2cbba
AhmedBakrXI <ahmedbakr2501@gmail.com>
2024-08-16 15:30:05 +0300

    update ui controller

--- src/main/java/org/ai/tictactoe/logic/player/ComputerPlayer.java
+++ src/main/java/org/ai/tictactoe/logic/player/ComputerPlayer.java
@@ -12,8 +12,10 @@ public class ComputerPlayer extends Player {
     }
 
     public void play(GameBoard b) {
+        System.out.println("Computer is thinking!");
         int[] move = {0, -1, -1};
         for (int depth = 1; depth <= difficulty; depth++) {
+            System.out.println("iterating..");
             int[] play = MinMax.optimalMove(b, Integer.MIN_VALUE, Integer.MAX_VALUE, depth, s);
             if (s == GameSymbol.O) {
                 if (move[1] == -1 || play[0] > move[0]) {
@@ -24,8 +26,7 @@ public class ComputerPlayer extends Player {
                     move = play;
                 }
             }
-            
-            // System.out.println("iterating");
+
             if (s == GameSymbol.O) {
                 if (move[0] == Integer.MAX_VALUE - 10) {
                     break;
@@ -37,15 +38,8 @@ public class ComputerPlayer extends Player {
             }
         }
 
-        // System.out.println("End iterative");
-        // if (move[1] == -1 || move[2] == -1) {
-        //     while (!b.isEmpty(move[1], move[2])) {
-        //         move[1] = new Random().nextInt() % 7;
-        //         move[2] = new Random().nextInt() % 7;
-        //     }
-        //     System.out.println("Random move");
-        // }
 
         b.place(s, move[1], move[2]);
+        System.out.println("Computer played\n");
     }
 }
