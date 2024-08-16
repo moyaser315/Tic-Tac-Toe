600a4c82a2ef6e8530fdeea1c20487109ec6cfec
AhmedBakrXI <ahmedbakr2501@gmail.com>
2024-08-16 14:22:02 +0300

    Restructuring the project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
-module org.ai.tictactoe {
+module org.ai.tictagtoe {
     requires javafx.controls;
     requires javafx.fxml;
 
 
     opens org.ai.tictactoe to javafx.fxml;
     exports org.ai.tictactoe;
+    exports org.ai.tictactoe.controllers;
+    opens org.ai.tictactoe.controllers to javafx.fxml;
 }
